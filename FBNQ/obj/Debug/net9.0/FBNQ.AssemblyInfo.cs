6de6663caa93e1aa20d6a6ae54bcef9a3c6a5720
tunji ty <tytunji29@gmail.com>
2025-05-22 00:05:30 +0100

    Done with the assessment

--- FBNQ/obj/Debug/net9.0/FBNQ.AssemblyInfo.cs
+++ FBNQ/obj/Debug/net9.0/FBNQ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FBNQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c482102880506d1994c8c2b87203266902973e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d51f1e3ed7280b48efca0cceeb11f648bf1484")]
 [assembly: System.Reflection.AssemblyProductAttribute("FBNQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FBNQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
